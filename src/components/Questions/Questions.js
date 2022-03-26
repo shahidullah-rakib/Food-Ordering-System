59dd865205f3cae5db97d5c02920b80c450990eb
shsidulla <shsidullarakib@gmail.com>
2022-03-26 15:41:22 +0600

    Answer the questions

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -13,7 +13,7 @@ const Questions = () => {
                     </h2>
                     <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                         <div className="accordion-body">
-                            <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            React is a JavaScript library that allows users to create user interfaces. React.js is commonly referred to as the application's view layer. React employs a declarative paradigm that makes it easy to reason about our application while still aiming to be efficient and adaptable. React creates views for each state in our project, and when our data changes, React updates and renders the appropriate component quickly. Using the declarative perspective, our code will be more predictable and easier to debug.
                         </div>
                     </div>
                 </div>
@@ -25,7 +25,33 @@ const Questions = () => {
                     </h2>
                     <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                         <div className="accordion-body">
-                            <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            <table>
+                                <tr>
+                                    <th>Serial No</th>
+                                    <th>Props</th>
+                                    <th>State</th>
+                                </tr>
+                                <tr>
+                                    <td>01</td>
+                                    <td>Props are read-only.</td>
+                                    <td>State changes can be asynchronous.</td>
+                                </tr>
+                                <tr>
+                                    <td>02</td>
+                                    <td>Props are immutable.</td>
+                                    <td>State is mutable.</td>
+                                </tr>
+                                <tr>
+                                    <td>03</td>
+                                    <td>The state is the local state of the component which cannot be accessed and modified outside of the component.</td>
+                                    <td>Props make components reusable by giving components the ability to receive data from their parent component in the form of props.</td>
+                                </tr>
+                                <tr>
+                                    <td>04</td>
+                                    <td>Props can be accessed by the child component.</td>
+                                    <td>State cannot be accessed by child components.</td>
+                                </tr>
+                            </table>
                         </div>
                     </div>
                 </div>
@@ -37,7 +63,7 @@ const Questions = () => {
                     </h2>
                     <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                         <div className="accordion-body">
-                            <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                            useState is a Hook that enables state variables to be used in functional components. This function takes the initial state and returns a variable with the current state value and another function to update it.
                         </div>
                     </div>
                 </div>
